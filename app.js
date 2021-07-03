3a2eeadcddb2ad55ee295830cb1b4ccaa504d9cc
Terro216 <0ilya@bk.ru>
2021-07-03 19:36:34 +0300

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './app.scss'
+
+function App() {
+    return(
+        <div className='app-wrapper'>
+            hello
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
